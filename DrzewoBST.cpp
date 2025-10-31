f601ccf07ad6547cb4f76e9fda299db032a009e6
Janek W <89817308+Janq20@users.noreply.github.com>
2025-10-31 09:12:23 +0100

    feat(BST): Implementacja struktury Wezel oraz dodawania elementów

--- DrzewoBST.cpp
+++ DrzewoBST.cpp
@@ -1,1 +1,28 @@
 #include "DrzewoBST.h"
+
+// --- Konstruktor ---
+DrzewoBST::DrzewoBST() {
+    korzen = nullptr; 
+}
+
+// --- Metody publiczne ---
+
+void DrzewoBST::dodaj(int wartosc) {
+    korzen = dodaj(korzen, wartosc);
+}
+
+// --- Metody prywatne ---
+
+Wezel* DrzewoBST::dodaj(Wezel* wezel, int wartosc) {
+    if (wezel == nullptr) {
+        return new Wezel(wartosc);
+    }
+    if (wartosc < wezel->dane) {
+        wezel->lewy = dodaj(wezel->lewy, wartosc);
+    }
+    else if (wartosc > wezel->dane) {
+        wezel->prawy = dodaj(wezel->prawy, wartosc);
+    }
+ 
+    return wezel;
+}
--- DrzewoBST.h
+++ DrzewoBST.h
@@ -1,11 +1,23 @@
-#pragma once 
+#pragma once
 #include <iostream>
-using namespace std; 
+
+using namespace std;
+
+struct Wezel {
+    int dane;
+    Wezel* lewy;
+    Wezel* prawy;
+    Wezel(int d) : dane(d), lewy(nullptr), prawy(nullptr) {}
+};
 
 class DrzewoBST {
 private:
-   
+    Wezel* korzen;
+
+    // Prywatna, rekurencyjna metoda pomocnicza do dodawania
+    Wezel* dodaj(Wezel* wezel, int wartosc);
 
 public:
-   
+    DrzewoBST(); 
+    void dodaj(int wartosc); 
 };
