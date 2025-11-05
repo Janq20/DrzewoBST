a8505bc63af98c1899fed848a58fa4bd5aa2ce6c
Janek W <89817308+Janq20@users.noreply.github.com>
2025-11-05 08:32:53 +0100

    feat(Pliki): Implementacja klasy MenedzerPlikow i wczytywania z pliku TXT

--- Pliki.cpp
+++ Pliki.cpp
@@ -1,1 +1,32 @@
 #include "Pliki.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// == Metoda Wczytywania z Tekstu ==
+void MenedzerPlikow::wczytajZTekstu(const string& nazwaPliku, DrzewoBST& drzewo) {
+    fstream plik;
+    plik.open(nazwaPliku, ios::in); 
+
+    if (!plik.is_open()) {
+        cout << "Blad otwarcia pliku do odczytu: " << nazwaPliku << endl;
+        return;
+    }
+
+    cout << "Rozpoczynam wczytywanie z pliku: " << nazwaPliku << "..." << endl;
+
+    int liczba;
+    while (plik >> liczba) {
+        drzewo.dodaj(liczba); 
+    }
+
+    if (plik.eof()) {
+        cout << "Wczytano liczby z pliku: " << nazwaPliku << endl;
+    } else if (plik.fail()) {
+        cout << "Blad formatu danych w pliku." << endl;
+    }
+
+    plik.close();
+}
--- Pliki.h
+++ Pliki.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include "DrzewoBST.h" 
 
 using namespace std;
 
 class MenedzerPlikow {
 private:
-    
 
 public:
-   
+    void wczytajZTekstu(const string& nazwaPliku, DrzewoBST& drzewo);
 };
