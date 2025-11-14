ca6e41c71ea9b0d71cb19e1d52a0474d156dd730
Janek W <89817308+Janq20@users.noreply.github.com>
2025-11-14 17:53:58 +0100

    Dodanie dokumentacji Doxygen do Pliki.cpp i DrzewoBST.cpp

--- DrzewoBST.cpp
+++ DrzewoBST.cpp
@@ -1,33 +1,72 @@
+/**
+ * @file DrzewoBST.cpp
+ * @brief Plik implementacyjny dla klasy DrzewoBST.
+ * @details Zawiera definicje metod klasy DrzewoBST, odpowiedzialnych za
+ * zarządzanie binarnym drzewem poszukiwań.
+ */
+
 #include "DrzewoBST.h"
 #include <fstream>
 #include <string>
 
 using namespace std;
 
+/**
+ * @brief Konstruktor domyślny klasy DrzewoBST.
+ * @details Inicjalizuje korzeń drzewa jako nullptr.
+ */
 DrzewoBST::DrzewoBST() {
     korzen = nullptr;
 }
 
+<<<<<<< HEAD
+=======
+/**
+ * @brief Destruktor klasy DrzewoBST.
+ * @details Automatycznie wywołuje metodę usunWszystko(), aby zwolnić pamięć
+ * przydzieloną na wszystkie węzły drzewa.
+ */
+>>>>>>> 91fc26d (Dodanie dokumentacji Doxygen do Pliki.cpp i DrzewoBST.cpp)
 DrzewoBST::~DrzewoBST() {
     usunWszystko();
 }
 
+<<<<<<< HEAD
+=======
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do dodawania węzła.
+ * @param wezel Wskaźnik na aktualny węzeł (lub poddrzewo).
+ * @param wartosc Wartość (klucz) do dodania do drzewa.
+ * @return Wskaźnik na węzeł po operacji dodania.
+ */
+>>>>>>> 91fc26d (Dodanie dokumentacji Doxygen do Pliki.cpp i DrzewoBST.cpp)
 Wezel* DrzewoBST::dodaj(Wezel* wezel, int wartosc) {
     if (wezel == nullptr) {
         return new Wezel(wartosc);
     }
     if (wartosc < wezel->dane) {
         wezel->lewy = dodaj(wezel->lewy, wartosc);
-    } else if (wartosc > wezel->dane) {
+    }
+    else if (wartosc > wezel->dane) {
         wezel->prawy = dodaj(wezel->prawy, wartosc);
     }
+    // Jeśli wartosc == wezel->dane, nic nie robimy (nie pozwalamy na duplikaty)
     return wezel;
 }
 
+/**
+ * @brief Publiczna metoda dodająca nową wartość do drzewa BST.
+ * @param wartosc Wartość (klucz) do dodania.
+ */
 void DrzewoBST::dodaj(int wartosc) {
     korzen = dodaj(korzen, wartosc);
 }
 
+/**
+ * @brief Prywatna funkcja pomocnicza do przechodzenia drzewa metodą Pre-order.
+ * @details (Korzeń -> Lewy -> Prawy)
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ */
 void DrzewoBST::preorder(Wezel* wezel) {
     if (wezel != nullptr) {
         cout << wezel->dane << " ";
@@ -36,6 +75,11 @@ void DrzewoBST::preorder(Wezel* wezel) {
     }
 }
 
+/**
+ * @brief Prywatna funkcja pomocnicza do przechodzenia drzewa metodą In-order.
+ * @details (Lewy -> Korzeń -> Prawy). Wyświetla elementy posortowane.
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ */
 void DrzewoBST::inorder(Wezel* wezel) {
     if (wezel != nullptr) {
         inorder(wezel->lewy);
@@ -44,6 +88,11 @@ void DrzewoBST::inorder(Wezel* wezel) {
     }
 }
 
+/**
+ * @brief Prywatna funkcja pomocnicza do przechodzenia drzewa metodą Post-order.
+ * @details (Lewy -> Prawy -> Korzeń)
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ */
 void DrzewoBST::postorder(Wezel* wezel) {
     if (wezel != nullptr) {
         postorder(wezel->lewy);
@@ -52,6 +101,11 @@ void DrzewoBST::postorder(Wezel* wezel) {
     }
 }
 
+/**
+ * @brief Publiczna metoda do wyświetlania drzewa na konsoli w wybranym trybie.
+ * @param tryb Liczba całkowita określająca tryb wyświetlania:
+ * 1: Pre-order, 2: In-order, 3: Post-order.
+ */
 void DrzewoBST::wyswietlGraficznie(int tryb) {
     if (korzen == nullptr) {
         cout << "Drzewo jest puste." << endl;
@@ -59,24 +113,31 @@ void DrzewoBST::wyswietlGraficznie(int tryb) {
     }
 
     switch (tryb) {
-        case 1:
-            cout << "Pre-order: ";
-            preorder(korzen);
-            break;
-        case 2:
-            cout << "In-order (posortowane): ";
-            inorder(korzen);
-            break;
-        case 3:
-            cout << "Post-order: ";
-            postorder(korzen);
-            break;
-        default:
-            cout << "Nieznany tryb wyświetlania." << endl;
+    case 1:
+        cout << "Pre-order: ";
+        preorder(korzen);
+        break;
+    case 2:
+        cout << "In-order (posortowane): ";
+        inorder(korzen);
+        break;
+    case 3:
+        cout << "Post-order: ";
+        postorder(korzen);
+        break;
+    default:
+        cout << "Nieznany tryb wyświetlania." << endl;
     }
-    cout << endl; 
+    cout << endl;
 }
 
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do znajdowania ścieżki do węzła.
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ * @param dane Wartość (klucz) szukanego węzła.
+ * @param sciezka Referencja do obiektu string, w którym budowana jest ścieżka.
+ * @return true jeśli węzeł został znaleziony, false w przeciwnym razie.
+ */
 bool DrzewoBST::znajdzSciezke(Wezel* wezel, int dane, string& sciezka) {
     if (wezel == nullptr) {
         return false;
@@ -87,46 +148,61 @@ bool DrzewoBST::znajdzSciezke(Wezel* wezel, int dane, string& sciezka) {
     if (wezel->dane == dane) {
         return true;
     }
-    
+
     sciezka += " -> ";
-    
+
     bool znaleziono;
-    
+
     if (dane < wezel->dane) {
         znaleziono = znajdzSciezke(wezel->lewy, dane, sciezka);
-    } else {
+    }
+    else {
         znaleziono = znajdzSciezke(wezel->prawy, dane, sciezka);
     }
 
+    // Jeśli nie znaleziono w poddrzewie, usuń ostatni dodany element i strzałkę
     if (!znaleziono) {
         string usunieta_wartosc = to_string(wezel->dane);
-        size_t dlugoscDoUsuniecia = usunieta_wartosc.length() + 4; 
+        size_t dlugoscDoUsuniecia = usunieta_wartosc.length() + 4; // " -> " ma 4 znaki
 
         if (sciezka.length() >= dlugoscDoUsuniecia && sciezka.substr(sciezka.length() - 4) == " -> ") {
-             sciezka.erase(sciezka.length() - dlugoscDoUsuniecia);
-        } else if (sciezka.length() >= usunieta_wartosc.length()) {
-             sciezka.erase(sciezka.length() - usunieta_wartosc.length());
+            sciezka.erase(sciezka.length() - dlugoscDoUsuniecia);
+        }
+        else if (sciezka.length() >= usunieta_wartosc.length()) {
+            // Ostatni element (bez strzałki)
+            sciezka.erase(sciezka.length() - usunieta_wartosc.length());
         }
     }
 
     return znaleziono;
 }
 
+/**
+ * @brief Publiczna metoda inicjująca szukanie ścieżki i wyświetlająca wynik.
+ * @param dane Wartość (klucz) węzła, do którego szukamy ścieżki.
+ */
 void DrzewoBST::znajdzSciezke(int dane) {
     string sciezka = "";
 
     if (znajdzSciezke(korzen, dane, sciezka)) {
         cout << "\nŚcieżka do danych " << dane << ": " << sciezka << endl;
-    } else {
+    }
+    else {
         cout << "\nDane " << dane << " nie zostały znalezione w drzewie." << endl;
     }
 }
 
+/**
+ * @brief Prywatna funkcja pomocnicza do znajdowania węzła o minimalnej wartości.
+ * @details Przechodzi w lewo aż do końca poddrzewa.
+ * @param wezel Wskaźnik na korzeń poddrzewa, w którym szukamy minimum.
+ * @return Wskaźnik na węzeł o minimalnej wartości w danym poddrzewie.
+ */
 Wezel* DrzewoBST::znajdzMin(Wezel* wezel) {
     if (wezel == nullptr) {
         return nullptr;
     }
-    
+
     Wezel* aktualny = wezel;
     while (aktualny->lewy != nullptr) {
         aktualny = aktualny->lewy;
@@ -134,6 +210,13 @@ Wezel* DrzewoBST::znajdzMin(Wezel* wezel) {
     return aktualny;
 }
 
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do usuwania węzła.
+ * @details Obsługuje trzy przypadki: brak dzieci, jedno dziecko, dwoje dzieci.
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ * @param wartosc Wartość (klucz) do usunięcia.
+ * @return Wskaźnik na węzeł (lub jego następcę) po operacji usunięcia.
+ */
 Wezel* DrzewoBST::usun(Wezel* wezel, int wartosc) {
     if (wezel == nullptr) {
         return nullptr;
@@ -141,40 +224,70 @@ Wezel* DrzewoBST::usun(Wezel* wezel, int wartosc) {
 
     if (wartosc < wezel->dane) {
         wezel->lewy = usun(wezel->lewy, wartosc);
-    } else if (wartosc > wezel->dane) {
+    }
+    else if (wartosc > wezel->dane) {
         wezel->prawy = usun(wezel->prawy, wartosc);
-    } else {
+    }
+    else {
+        // Znaleziono węzeł do usunięcia
+        // Przypadek 1: Brak lub jedno dziecko
         if (wezel->lewy == nullptr) {
             Wezel* temp = wezel->prawy;
             delete wezel;
             return temp;
-        } else if (wezel->prawy == nullptr) {
+        }
+        else if (wezel->prawy == nullptr) {
             Wezel* temp = wezel->lewy;
             delete wezel;
             return temp;
         }
 
+        // Przypadek 2: Dwoje dzieci
+        // Znajdź następnika (najmniejszy w prawym poddrzewie)
         Wezel* nastepnik = znajdzMin(wezel->prawy);
+        // Skopiuj dane następnika do tego węzła
         wezel->dane = nastepnik->dane;
+        // Usuń następnika (rekurencyjnie)
         wezel->prawy = usun(wezel->prawy, nastepnik->dane);
     }
-    
-    return wezel; 
+
+    return wezel;
 }
 
+/**
+ * @brief Publiczna metoda usuwająca węzeł o podanej wartości z drzewa.
+ * @param wartosc Wartość (klucz) do usunięcia.
+ */
 void DrzewoBST::usun(int wartosc) {
     Wezel* stary_korzen = korzen;
     korzen = usun(korzen, wartosc);
 
+    // Komunikaty dla użytkownika
     if (stary_korzen == nullptr) {
+<<<<<<< HEAD
          cout << "\nNie znaleziono wartości " << wartosc << " do usunięcia (drzewo puste)." << endl;
     } else if (korzen == nullptr && stary_korzen != nullptr) {
          cout << "\nWartość " << wartosc << " usunięta pomyśllnie. Drzewo jest teraz puste." << endl;
     } else {
          cout << "\nOperacja usuwania dla " << wartosc << " zakończona." << endl;
+=======
+        cout << "\nNie znaleziono wartości " << wartosc << " do usunięcia (drzewo puste)." << endl;
+    }
+    else if (korzen == nullptr && stary_korzen != nullptr) {
+        cout << "\nWartość " << wartosc << " usunięta pomyśllnie. Drzewo jest teraz puste." << endl;
+    }
+    else {
+        cout << "\nOperacja usuwania dla " << wartosc << " zakończona." << endl;
+>>>>>>> 91fc26d (Dodanie dokumentacji Doxygen do Pliki.cpp i DrzewoBST.cpp)
     }
 }
 
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do zapisu drzewa do pliku tekstowego.
+ * @details Używa kolejności Pre-order, aby ułatwić odtworzenie tej samej struktury.
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ * @param plik Referencja do otwartego strumienia pliku (fstream).
+ */
 void DrzewoBST::zapiszDoTekstu(Wezel* wezel, fstream& plik) {
     if (wezel != nullptr) {
         plik << wezel->dane << "\n";
@@ -183,10 +296,14 @@ void DrzewoBST::zapiszDoTekstu(Wezel* wezel, fstream& plik) {
     }
 }
 
+/**
+ * @brief Publiczna metoda zapisująca zawartość drzewa do pliku tekstowego.
+ * @param nazwaPliku Nazwa pliku (ścieżka), do którego drzewo ma być zapisane.
+ */
 void DrzewoBST::zapiszDoTekstu(const string& nazwaPliku) {
     fstream plik;
-    plik.open(nazwaPliku, ios::out); 
-    
+    plik.open(nazwaPliku, ios::out);
+
     if (!plik.is_open()) {
         cout << "Błąd otwarcia pliku do zapisu: " << nazwaPliku << endl;
         return;
@@ -198,15 +315,37 @@ void DrzewoBST::zapiszDoTekstu(const string& nazwaPliku) {
     cout << "Drzewo zapisane (tekstowo) do pliku: " << nazwaPliku << endl;
 }
 
+<<<<<<< HEAD
 void DrzewoBST::usunWszystko() {
     usunWszystko(korzen); 
     korzen = nullptr;     
 }
 
+=======
+/**
+ * @brief Publiczna metoda usuwająca wszystkie węzły z drzewa.
+ * @details Ustawia korzeń na nullptr po zwolnieniu pamięci.
+ */
+void DrzewoBST::usunWszystko() {
+    usunWszystko(korzen);
+    korzen = nullptr;
+}
+
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do usuwania wszystkich węzłów.
+ * @details Używa kolejności Post-order, aby poprawnie zwolnić pamięć
+ * (najpierw dzieci, potem rodzic).
+ * @param wezel Wskaźnik na aktualnie przetwarzany węzeł.
+ */
+>>>>>>> 91fc26d (Dodanie dokumentacji Doxygen do Pliki.cpp i DrzewoBST.cpp)
 void DrzewoBST::usunWszystko(Wezel* wezel) {
     if (wezel != nullptr) {
         usunWszystko(wezel->lewy);
         usunWszystko(wezel->prawy);
         delete wezel;
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 91fc26d (Dodanie dokumentacji Doxygen do Pliki.cpp i DrzewoBST.cpp)
--- Pliki.cpp
+++ Pliki.cpp
@@ -1,13 +1,30 @@
+/**
+ * @file Pliki.cpp
+ * @brief Plik implementacyjny dla klasy MenedzerPlikow.
+ * @details Zawiera definicje metod odpowiedzialnych za zapis i odczyt
+ * drzewa BST do/z plików tekstowych oraz binarnych.
+ */
+
 #include "Pliki.h"
 #include <fstream>
 #include <iostream>
 #include <string>
 
+ // Wymagany do dzia³ania, poniewa¿ Pliki.h go zawiera
+#include "DrzewoBST.h" 
+
 using namespace std;
 
+/**
+ * @brief Wczytuje drzewo z pliku tekstowego.
+ * @details Otwiera plik tekstowy, czyta liczby (jedna na liniê lub oddzielone spacj¹)
+ * i dodaje je do drzewa.
+ * @param nazwaPliku Œcie¿ka do pliku wejœciowego.
+ * @param drzewo Referencja do obiektu DrzewoBST, który ma byæ wype³niony danymi.
+ */
 void MenedzerPlikow::wczytajZTekstu(const string& nazwaPliku, DrzewoBST& drzewo) {
     fstream plik;
-    plik.open(nazwaPliku, ios::in); 
+    plik.open(nazwaPliku, ios::in);
 
     if (!plik.is_open()) {
         cout << "Blad otwarcia pliku do odczytu: " << nazwaPliku << endl;
@@ -18,33 +35,53 @@ void MenedzerPlikow::wczytajZTekstu(const string& nazwaPliku, DrzewoBST& drzewo)
 
     int liczba;
     while (plik >> liczba) {
-        drzewo.dodaj(liczba); 
+        drzewo.dodaj(liczba);
     }
 
     if (plik.eof() && !plik.fail()) {
         cout << "Wczytano liczby z pliku: " << nazwaPliku << endl;
-    } else if (plik.fail()) {
+    }
+    else if (plik.fail()) {
         cout << "Blad formatu danych w pliku." << endl;
     }
 
     plik.close();
 }
 
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do zapisu binarnego.
+ * @details Zapisuje drzewo w kolejnoœci Pre-order. Dla ka¿dego wêz³a
+ * zapisuje flagê (bool), czy wêze³ istnieje (true) czy jest to nullptr (false).
+ * Jeœli istnieje, zapisuje równie¿ jego dane (int).
+ * @param plik Referencja do otwartego strumienia pliku binarnego (fstream).
+ * @param wezel WskaŸnik na aktualnie przetwarzany wêze³.
+ */
 void MenedzerPlikow::zapiszWezelBinarnie(fstream& plik, Wezel* wezel) {
     if (wezel == nullptr) {
+        // Zapisz 'false', aby zaznaczyæ brak wêz³a (nullptr)
         bool jestWezel = false;
         plik.write(reinterpret_cast<char*>(&jestWezel), sizeof(bool));
-    } else {
+    }
+    else {
+        // Zapisz 'true', aby zaznaczyæ istnienie wêz³a
         bool jestWezel = true;
         plik.write(reinterpret_cast<char*>(&jestWezel), sizeof(bool));
-        
+
+        // Zapisz dane wêz³a
         plik.write(reinterpret_cast<char*>(&wezel->dane), sizeof(int));
-        
+
+        // Kontynuuj rekurencyjnie
         zapiszWezelBinarnie(plik, wezel->lewy);
         zapiszWezelBinarnie(plik, wezel->prawy);
     }
 }
 
+/**
+ * @brief Publiczna metoda zapisuj¹ca ca³¹ strukturê drzewa do pliku binarnego.
+ * @details Wymaga przyjaŸni z klas¹ DrzewoBST, aby uzyskaæ dostêp do korzenia.
+ * @param nazwaPliku Œcie¿ka do pliku wyjœciowego.
+ * @param drzewo Referencja do obiektu DrzewoBST, który ma byæ zapisany.
+ */
 void MenedzerPlikow::zapiszBinarnie(const string& nazwaPliku, DrzewoBST& drzewo) {
     fstream plik;
     plik.open(nazwaPliku, ios::out | ios::binary);
@@ -56,17 +93,25 @@ void MenedzerPlikow::zapiszBinarnie(const string& nazwaPliku, DrzewoBST& drzewo)
 
     cout << "Rozpoczynam zapis binarny do pliku: " << nazwaPliku << "..." << endl;
     
+    // U¿ywamy 'drzewo.korzen' dziêki deklaracji 'friend class' w DrzewoBST.h
     zapiszWezelBinarnie(plik, drzewo.korzen);
-    
+
     plik.close();
     cout << "Drzewo zapisane binarnie do pliku: " << nazwaPliku << endl;
 }
 
+/**
+ * @brief Prywatna, rekurencyjna funkcja pomocnicza do odczytu binarnego.
+ * @details Odczytuje flagê (bool). Jeœli jest 'true', odczytuje dane (int),
+ * tworzy nowy wêze³ i rekurencyjnie odbudowuje jego lewe i prawe poddrzewo.
+ * Jeœli jest 'false', zwraca nullptr.
+ * @param plik Referencja do otwartego strumienia pliku binarnego (fstream).
+ * @return WskaŸnik na nowo utworzony wêze³ lub nullptr.
+ */
 Wezel* MenedzerPlikow::wczytajWezelBinarnie(fstream& plik) {
     bool jestWezel;
-    
-    plik.read(reinterpret_cast<char*>(&jestWezel), sizeof(bool));
 
+    plik.read(reinterpret_cast<char*>(&jestWezel), sizeof(bool));
     if (!plik) {
         return nullptr;
     }
@@ -79,13 +124,22 @@ Wezel* MenedzerPlikow::wczytajWezelBinarnie(fstream& plik) {
         
         nowyWezel->lewy = wczytajWezelBinarnie(plik);
         nowyWezel->prawy = wczytajWezelBinarnie(plik);
-        
+
         return nowyWezel;
-    } else {
+    }
+    else {
+        // To by³ nullptr
         return nullptr;
     }
 }
 
+/**
+ * @brief Publiczna metoda wczytuj¹ca strukturê drzewa z pliku binarnego.
+ * @details Przed wczytaniem, ta metoda wywo³uje `drzewo.usunWszystko()`,
+ * aby wyczyœciæ aktualn¹ zawartoœæ drzewa.
+ * @param nazwaPliku Œcie¿ka do pliku binarnego.
+ * @param drzewo Referencja do obiektu DrzewoBST, który zostanie nadpisany.
+ */
 void MenedzerPlikow::wczytajBinarnie(const string& nazwaPliku, DrzewoBST& drzewo) {
     fstream plik;
     plik.open(nazwaPliku, ios::in | ios::binary);
@@ -97,17 +151,23 @@ void MenedzerPlikow::wczytajBinarnie(const string& nazwaPliku, DrzewoBST& drzewo
 
     cout << "Rozpoczynam wczytywanie binarne z pliku: " << nazwaPliku << "..." << endl;
 
+    // Wyczyœæ istniej¹ce drzewo przed wczytaniem nowego
     drzewo.usunWszystko();
 
+    // Odbuduj drzewo i ustaw nowy korzeñ (wymaga przyjaŸni)
     drzewo.korzen = wczytajWezelBinarnie(plik);
 
+    // Sprawdzenie, czy plik zosta³ odczytany w ca³oœci
     if (plik.peek() == EOF && plik.eof()) {
         cout << "Wczytano (binarnie) drzewo z pliku: " << nazwaPliku << endl;
-    } else if (plik.fail()) {
+    }
+    else if (plik.fail()) {
         cout << "Blad formatu danych w pliku binarnym." << endl;
-    } else if (!plik.eof()) {
+    }
+    else if (!plik.eof()) {
         cout << "Ostrzezenie: Plik binarny nie zostal odczytany do konca." << endl;
-    } else {
+    }
+    else {
         cout << "Wczytano (binarnie) drzewo z pliku: " << nazwaPliku << endl;
     }
 
